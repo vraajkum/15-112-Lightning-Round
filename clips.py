7b7bcee23a1ada6ce98e4509dd9528106e54ed91
Veekant <veekant@gmail.com>
2025-05-06 00:33:03 -0400

    small style changes

--- clips.py
+++ clips.py
@@ -1,14 +1,70 @@
 import config
-import os, sys, subprocess
+import os, csv, sys, subprocess
 from PIL import Image, ImageDraw, ImageFont
 
+def createClips(csvPath, debug):
+    with open(csvPath, 'r') as csvFile:
+        csvReader = csv.reader(csvFile)
+        failureInfo = dict()
+
+        for line in csvReader:
+            processID(line, failureInfo, debug)
+            print()
+
+    displayFailureInfo(failureInfo)
+
+def processID(info, failureInfo, debug):
+    id, title, url, start, end = info
+    print(f'ID: {id}')
+
+    print('Downloading...')
+    if download(id, url, debug):
+        print('\tSucceeded')
+    else:
+        print('\tFailed')
+        failureInfo.get('Download', []).append(id)
+        return
+
+    print('Trimming...')
+    if trim(id, start, end, debug):
+        print('\tSucceeded')
+    else:
+        print("\tFailed")
+        failureInfo.get('Trim', []).append(id)
+        return
+
+    print('Creating frame...')
+    if createFrame(id, title):
+        print('\tSucceeded')
+    else:
+        print("\tFailed")
+        failureInfo.get('Frame', []).append(id)
+        return
+
+    print('Overlaying...')
+    if overlay(id, debug):
+        print('\tSucceeded')
+    else:
+        print("\tFailed")
+        failureInfo.get('Overlay', []).append(id)
+        return
+    
+def displayFailureInfo(failureInfo):
+    if len(failureInfo) == 0:
+        print('No failures!')
+    else:
+       for failureType in failureInfo:
+           failures = failureInfo[failureType]
+           print(f'{failureType}: {', '.join(failures)}') 
+
 def download(id, url, debug):
-    outPath = os.path.join(config.fullVideoDir, f'{id}.{config.videoFormat}')
+    format = config.videoFormat
+    outPath = os.path.join(config.fullVideoDir, f'{id}.{format}')
     if os.path.isfile(outPath):
         return True
 
     binary = f'{sys.executable} -m yt_dlp'
-    cmd = binary + f' -f {config.videoFormat} -o {outPath} {url} --no-check-certificate'
+    cmd = binary + f' -f {format} -o {outPath} {url} --no-check-certificate'
 
     try:
         if debug:
@@ -22,17 +78,18 @@ def download(id, url, debug):
         return False
 
 def trim(id, start, end, debug):
-    inPath = os.path.join(config.fullVideoDir, f'{id}.{config.videoFormat}')
-    outPath = os.path.join(config.trimmedVideoDir, f'{id}.{config.videoFormat}')
+    format = config.videoFormat
+    inPath = os.path.join(config.fullVideoDir, f'{id}.{format}')
+    outPath = os.path.join(config.trimmedVideoDir, f'{id}.{format}')
     if not os.path.isfile(inPath):
         return False
     if os.path.isfile(outPath):
         return True
 
-    startTime = mmssToSeconds(start)
-    endTime = mmssToSeconds(end)
-    command = f'''ffmpeg -i {inPath} -ss {startTime} -to {endTime} -an '''
-    command += f'''-vf scale={config.width}:{config.height},pad={config.width}:{config.height}:(ow-iw)/2:(oh-ih)/2 {outPath}'''
+    startTime, endTime = mmssToSeconds(start), mmssToSeconds(end)
+    w, h = config.width, config.height
+    command = f'ffmpeg -i {inPath} -ss {startTime} -to {endTime} -an '
+    command += f'-vf scale={w}:{h},pad={w}:{h}:(ow-iw)/2:(oh-ih)/2 {outPath}'
     try:
         if debug:
             print(command)
@@ -66,7 +123,8 @@ def createFrame(id, title):
     if width > maxWidth:
         print(f'''"{title}" is too long, frame not created.''')
         return False
-    draw.rectangle([0, 800, 150+width+50, 930], fill = "white", outline = (0,0,0,0), width = 0)
+    draw.rectangle([0, 800, 150+width+50, 930], fill = "white",
+                   outline = (0,0,0,0), width = 0)
     draw.text((165, 800), title, font=font, fill=color)
     draw.text((0, 750), config.currSem, font=smallFont, fill=color)
     draw.rectangle([0,800, 150, 930], fill=color)
@@ -77,15 +135,19 @@ def createFrame(id, title):
     return True
 
 def overlay(id, debug):
-    framePath = os.path.join(config.frameDir, f'{id}.{config.imageFormat}')
-    clipPath = os.path.join(config.trimmedVideoDir, f'{id}.{config.videoFormat}')
-    outPath = os.path.join(config.overlaidVideoDir, f'{id}.{config.videoFormat}')
+    imgFormat = config.imageFormat
+    vidFormat = config.videoFormat
+
+    framePath = os.path.join(config.frameDir, f'{id}.{imgFormat}')
+    clipPath = os.path.join(config.trimmedVideoDir, f'{id}.{vidFormat}')
+    outPath = os.path.join(config.overlaidVideoDir, f'{id}.{vidFormat}')
     if not os.path.isfile(framePath) or not os.path.isfile(clipPath):
         return False
     if os.path.isfile(outPath):
         return True
 
-    command = f"ffmpeg -i {clipPath} -i {framePath} -filter_complex [0][1]overlay=x=0:y=0 {outPath}"
+    command = f'ffmpeg -i {clipPath} -i {framePath} '
+    command += f'-filter_complex [0][1]overlay=x=0:y=0 {outPath}'
 
     try:
         if debug:
--- main.py
+++ main.py
@@ -16,23 +16,15 @@ vraajkum,coolTP,https://www.youtube.com/watch?v=dQw4w9WgXcQ,1:12,1:16
 '''
 
 import config
-from createClips import download, trim, createFrame, overlay
+from clips import createClips
 import argparse, os, csv
 
 def main(csvPath, debug):
     terminalSize = os.get_terminal_size().columns
     createMissingDirs()
 
-    with open(csvPath, 'r') as csvFile:
-        csvReader = csv.reader(csvFile)
-        failureInfo = dict()
-
-        print(('-' * terminalSize) + '\n')
-        for line in csvReader:
-            processID(line, failureInfo, debug)
-            print()
-
-    displayFailureInfo(failureInfo)
+    print(('-' * terminalSize) + '\n')
+    createClips(csvPath, debug)
     print('-' * terminalSize)
 
 def createMissingDirs():
@@ -41,50 +33,6 @@ def createMissingDirs():
     for dir in dirs:
         if not os.path.exists(dir):
             os.makedirs(dir)
-
-def processID(info, failureInfo, debug):
-    id, title, url, start, end = info
-    print(f'ID: {id}')
-
-    print('Downloading...')
-    if download(id, url, debug):
-        print('\tSucceeded')
-    else:
-        print('\tFailed')
-        failureInfo.get('Download', []).append(id)
-        return
-
-    print('Trimming...')
-    if trim(id, start, end, debug):
-        print('\tSucceeded')
-    else:
-        print("\tFailed")
-        failureInfo.get('Trim', []).append(id)
-        return
-
-    print('Creating frame...')
-    if createFrame(id, title):
-        print('\tSucceeded')
-    else:
-        print("\tFailed")
-        failureInfo.get('Frame', []).append(id)
-        return
-
-    print('Overlaying...')
-    if overlay(id, debug):
-        print('\tSucceeded')
-    else:
-        print("\tFailed")
-        failureInfo.get('Overlay', []).append(id)
-        return
-    
-def displayFailureInfo(failureInfo):
-    if len(failureInfo) == 0:
-        print('No failures!')
-    else:
-       for failureType in failureInfo:
-           failures = failureInfo[failureType]
-           print(f'{failureType}: {', '.join(failures)}') 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
